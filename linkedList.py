93e8c680b32f48bcf702aa6c9ef7244af86a46f2
Ankit Mehra <ankitmehra1986@gmail.com>
2022-07-09 12:02:21 -0400

    double & single linked list with all functions

--- linkedList.py
+++ linkedList.py
@@ -6,7 +6,7 @@ class Node:
         return f"Node({self.data})"
     
 class LinkedList:
-    def __init__(self,nodes:list[Node]) -> None:
+    def __init__(self,nodes=None) -> None:
         self.head  = None
         self.tail = None
         if nodes:
@@ -17,18 +17,13 @@ class LinkedList:
                 node = obj
             self.tail = node
     
-    def __iter__(self):
-        
-        temp = self.head
-        while temp:
-            yield temp
-            temp = temp.next
+    
     
     def __repr__(self) -> str:
         return '->'.join([str(item) for item in iter(self)])
 
     def __len__(self):
-        return tuple(self)
+        return len(tuple(iter(self)))
 
     def append(self,value):
         temp = Node(value)
@@ -38,6 +33,126 @@ class LinkedList:
         else:
             self.tail.next = temp
             self.tail = temp
+            
+    def pop(self):
+        temp = self.head
+        if len(self) == 0:
+            return None
+        else:
+            pre = self.head
+            while(temp.next):
+                pre = temp
+                temp = temp.next
+            self.tail = pre
+            self.tail.next = None
+        if len(self)< 2:
+                self.head = None
+                self.tail = None
+        return temp
+    
+    def prepend(self,value):
+        temp = Node(value)
+        if len(self) == 0:
+            self.head = temp
+            self.tail = temp
+        else:
+            temp.next = self.head
+            self.head = temp
+        
+    def pop_first(self):
+        if len(self) == 0:
+            return None
+        temp = self.head
+        self.head = temp.next
+        temp.next  = None
+        if len(self) == 0:
+            self.tail = None
+        return temp
+    
+    def get(self,index:int):
+        if not 0 <= index <= len(self):
+            raise ValueError('Index out of range')
+        temp = self.head
+        for _ in range(index):
+            temp = temp.next
+        return temp
+                
+                
+    def set_value(self,index,value):
+        if not 0 <= index <= len(self):
+            raise ValueError('Index out of range')
+        temp = self.get(index)
+        if temp:
+            temp.data = value
+            return True
+        return False
+
+    def insert(self,index,value):
+        if not 0 <= index <= len(self):
+            raise ValueError('Index out of range')
+        if index == 0:
+            return self.prepend(value)
+        elif index == len(self):
+            return self.append(value)
+        temp = self.get(index-1)
+        new_node = Node(value)
+
+        new_node.next = temp.next
+        temp.next = new_node
+        return True
+            
+    def remove(self, index):
+        if not 0 <= index <= len(self):
+            raise ValueError('Index out of range')
+        if index == 0:
+            return self.pop_first()
+        elif index == len(self)-1:
+            return self.pop()
+        pre = self.get(index-1)
+        temp = pre.next
+        pre.next = temp.next
+        temp.next = None
+        return temp
+        
+    def reverse(self):
+        temp = self.head
+        self.head = self.tail
+        self.tail = temp
+        before = None
+        for _ in range(len(self)):
+            after = temp.next
+            temp.next = before
+            before = temp
+            temp = after
+            
+    def reverse2(self) -> None:
+        """
+        This reverses the linked list order.
+        >>> linked_list = LinkedList()
+        >>> linked_list.insert_tail("first")
+        >>> linked_list.insert_tail("second")
+        >>> linked_list.insert_tail("third")
+        >>> linked_list
+        first->second->third
+        >>> linked_list.reverse()
+        >>> linked_list
+        third->second->first
+        """
+        prev = None
+        current = self.head
+        self.tail = current
+
+        while current:
+            # Store the current node's next node.
+            next_node = current.next
+            # Make the current node's next point backwards
+            current.next = prev
+            # Make the previous node be the current node
+            prev = current
+            # Make the current node the next node (to progress iteration)
+            current = next_node
+        # Return prev in order to put the head at the end
+        self.head = prev
             
 def main():
     some_nodes = [Node('Toronto'),
@@ -51,6 +166,26 @@ def main():
     print(first_LL.tail)
     first_LL.append('Markham')
     print(first_LL)
+    poped = first_LL.pop()
+    print(poped)
+    print(first_LL)
+    print(first_LL.head)
+    print(first_LL.tail)
+    first_LL.prepend('Markham')
+    print(first_LL)
+    print(first_LL.pop_first())
+    print(first_LL)
+    print(first_LL.get(1))
+    first_LL.set_value(1,'Peel')
+    print(first_LL)
+    first_LL.insert(3,'Scarborough')
+    print(first_LL)
+    first_LL.remove(1)
+    print(first_LL)
+    first_LL.reverse2()
+    print(first_LL)
+    print(first_LL.tail)
+    
     
 if __name__ == "__main__":
     main()
